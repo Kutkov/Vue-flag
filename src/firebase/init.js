26da84d5c1d46e41530847f9d3c821d43b229cce
Максим Кутьков <maksimkutkov@MacBook-Pro-Maksim.local>
2018-11-26 02:54:48 +0300

    mvp flag app

---
+++ src/firebase/init.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase'
+
+var config = {
+  apiKey: "AIzaSyCN8rddAgUd1pwd7ukXpR3_YSatxTJpLbE",
+  authDomain: "vue-flag.firebaseapp.com",
+  databaseURL: "https://vue-flag.firebaseio.com",
+  projectId: "vue-flag",
+  storageBucket: "vue-flag.appspot.com",
+  messagingSenderId: "642592421169"
+};
+const firebaseApp = firebase.initializeApp(config);
+
+firebaseApp.firestore().settings({timestampsInSnapshots: true})
+
+export default firebaseApp.firestore()
